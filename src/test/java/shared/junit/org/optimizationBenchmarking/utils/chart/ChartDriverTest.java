d70069c1b060c12cb4bbafc41dd3e16bbc9bc499
Thomas Weise <tweise@ustc.edu.cn>
2016-01-03 13:59:59 +0800

    Added Base Class for Testing Chart Drivers

---
+++ src/test/java/shared/junit/org/optimizationBenchmarking/utils/chart/ChartDriverTest.java
@@ -0,0 +1,17 @@
+package shared.junit.org.optimizationBenchmarking.utils.chart;
+
+import org.junit.Ignore;
+import org.optimizationBenchmarking.utils.chart.spec.IChartDriver;
+
+import shared.junit.org.optimizationBenchmarking.utils.tools.ToolTest;
+
+/** Test a chart driver can be used */
+@Ignore
+public abstract class ChartDriverTest extends ToolTest<IChartDriver> {
+
+  /** create */
+  public ChartDriverTest() {
+    super();
+  }
+
+}
---
+++ src/test/java/shared/junit/org/optimizationBenchmarking/utils/chart/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * <p>
+ * Here we test the utility classes of the <a
+ * href="http://www.optimizationBenchmarking.org/"
+ * >optimizationBenchmarking.org</a> tool suite in package
+ * {@link org.optimizationBenchmarking.utils.chart}.
+ * </p>
+ */
+package shared.junit.org.optimizationBenchmarking.utils.chart;

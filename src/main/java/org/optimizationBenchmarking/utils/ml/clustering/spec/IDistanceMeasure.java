c74c357ba79797668025ff15ac3909cd60a5d39c
Thomas Weise <tweise@ustc.edu.cn>
2016-01-11 16:32:31 +0800

    Added isInteger() to IDistanceMeasure
    The distance measure can now state whether it usually produces integer
    numbers or not. This can help to optimize distance matrices generated
    via the distance matrix builder API.

--- src/main/java/org/optimizationBenchmarking/utils/ml/clustering/spec/IDistanceMeasure.java
+++ src/main/java/org/optimizationBenchmarking/utils/ml/clustering/spec/IDistanceMeasure.java
@@ -22,4 +22,15 @@ public interface IDistanceMeasure {
   public abstract double compute(final IMatrix matrixA, final int rowA,
       final IMatrix matrixB, final int rowB);
 
+  /**
+   * Will this {@linkplain #compute(IMatrix, int, IMatrix, int) distance
+   * measure} usually produce integer results ({@code true}) or floating
+   * point results ({@code false})? This can be used to optimize backing
+   * data structures.
+   *
+   * @return {@code true} if {@link #compute(IMatrix, int, IMatrix, int)
+   *         distance measure} usually returns integer numbers,
+   *         {@code false} otherwise
+   */
+  public abstract boolean isInteger();
 }

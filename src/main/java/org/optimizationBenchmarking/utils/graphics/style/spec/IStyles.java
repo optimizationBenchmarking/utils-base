27573356eb6d7e38c3cdbcc4a12401a371c60378
Thomas Weise <tweise@home>
2016-01-14 05:32:14 +0800

    Added getColor, getStroke, and getFont to IStyles
    The IStyles interface now allows for getting named color-, stroke-, and
    font styles. They will be allocated if not yet specified.

--- src/main/java/org/optimizationBenchmarking/utils/graphics/style/spec/IStyles.java
+++ src/main/java/org/optimizationBenchmarking/utils/graphics/style/spec/IStyles.java
@@ -30,4 +30,58 @@ public interface IStyles extends IBasicStyles {
    * @return the stroke style
    */
   public abstract IStrokeStyle allocateStroke();
+
+  /**
+   * Obtain the color of a given name. If {@code allocateIfUndefined} is
+   * {@code true} and no color is defined for the given name yet, then one
+   * will be allocated. If {@code allocateIfUndefined} is {@code false} and
+   * no color is defined for the given name yet, {@code null} will be
+   * returned.
+   *
+   * @param name
+   *          the name
+   * @param allocateIfUndefined
+   *          should the color be allocated if it is still undefined?
+   * @return the color for the given name, or {@code null} if none was
+   *         allocated for the name and {@code allocateIfUndefined} is
+   *         {@code false}
+   */
+  public abstract IColorStyle getColor(final String name,
+      final boolean allocateIfUndefined);
+
+  /**
+   * Obtain the stroke of a given name. If {@code allocateIfUndefined} is
+   * {@code true} and no stroke is defined for the given name yet, then one
+   * will be allocated. If {@code allocateIfUndefined} is {@code false} and
+   * no stroke is defined for the given name yet, {@code null} will be
+   * returned.
+   *
+   * @param name
+   *          the name
+   * @param allocateIfUndefined
+   *          should the stroke be allocated if it is still undefined?
+   * @return the stroke for the given name, or {@code null} if none was
+   *         allocated for the name and {@code allocateIfUndefined} is
+   *         {@code false}
+   */
+  public abstract IStrokeStyle getStroke(final String name,
+      final boolean allocateIfUndefined);
+
+  /**
+   * Obtain the font of a given name. If {@code allocateIfUndefined} is
+   * {@code true} and no font is defined for the given name yet, then one
+   * will be allocated. If {@code allocateIfUndefined} is {@code false} and
+   * no font is defined for the given name yet, {@code null} will be
+   * returned.
+   *
+   * @param name
+   *          the name
+   * @param allocateIfUndefined
+   *          should the font be allocated if it is still undefined?
+   * @return the font for the given name, or {@code null} if none was
+   *         allocated for the name and {@code allocateIfUndefined} is
+   *         {@code false}
+   */
+  public abstract IFontStyle getFont(final String name,
+      final boolean allocateIfUndefined);
 }

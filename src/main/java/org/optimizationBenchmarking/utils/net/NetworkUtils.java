00020c92963fb52c741200392262181e0a851701
Thomas Weise <tweise@ustc.edu.cn>
2016-05-03 10:37:01 +0800

    Improved way to Determine Global Network Addresses
    In our NetworkUtils, we try to obtain the address under which the
    current computer can be seen in the internet. We now do this in two
    steps:
    
    1. We query existing web pages that simply print the IP address from
       which they receive the request. This way we get the address under
       which outside computers see requests from the current computer.
       
    2. We start a UDP server at a random port of the local computer. For
       each address obtained under point 1, we send a message to the UDP
       server. If it is received, we know that the address very likely
       belongs to our computer is not NATed. Of course, we may be unlucky
       with NAT and port forwards, but this is as good as we can get.

--- src/main/java/org/optimizationBenchmarking/utils/net/NetworkUtils.java
+++ src/main/java/org/optimizationBenchmarking/utils/net/NetworkUtils.java
@@ -1,21 +1,24 @@
 package org.optimizationBenchmarking.utils.net;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.HttpURLConnection;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.MalformedURLException;
 import java.net.NetworkInterface;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.Random;
 import java.util.logging.Level;
 
 import org.optimizationBenchmarking.utils.collections.iterators.EnumerationIterator;
@@ -53,7 +56,28 @@ public final class NetworkUtils {
 
   /**
    * Obtain the set of internet addresses of this host as seen from the
-   * outside world.
+   * outside world. Each such address has two features:
+   * <ol>
+   * <li>At least computer outside the local network of the present
+   * computer sees this address if we send a message to it.</li>
+   * <li>If we send a (UDP) message to the address, it can be received by a
+   * server running on this computer.</li>
+   * </ol>
+   * <p>
+   * This is achieved as follows: we try to obtain the address under which
+   * the current computer can be seen in the internet. We now do this in
+   * two steps:
+   * </p>
+   * <ol>
+   * <li>We query existing web pages that simply print the IP address from
+   * which they receive the request. This way we get the address under
+   * which outside computers see requests from the current computer.</li>
+   * <li>We start a UDP server at a random port of the local computer. For
+   * each address obtained under point 1, we send a message to the UDP
+   * server. If it is received, we know that the address very likely
+   * belongs to our computer is not NATed. Of course, we may be unlucky
+   * with NAT and port forwards, but this is as good as we can get.</li>
+   * </ol>
    *
    * @return the set of internet addresses under which this host is seen by
    *         the outside world.
@@ -63,10 +87,18 @@ public final class NetworkUtils {
   }
 
   /**
+   * <p>
    * Get the most-likely public address of this computer, i.e., the address
    * which most-likely can be used to access server processes running on
    * this computer from the outside world. There is no guarantee that this
    * is actually the IP address which can be used for accessing the server.
+   * </p>
+   * <p>
+   * In the ideal case, it would be an address which occurs in both
+   * {@link #getGlobalAddresses()} and {@link #getLocalAddresses()}. But
+   * since either of these lists may be empty, it may also just be the
+   * local loop-back address.
+   * </p>
    *
    * @return the most likely used public address of this server
    * @see #getMostLikelyPublicName()
@@ -165,6 +197,31 @@ public final class NetworkUtils {
           + " (represented as '") + useHost + //$NON-NLS-1$
           "') and port ")//$NON-NLS-1$
           + port) + '\'') + '.'), error);
+    }
+  }
+
+  /**
+   * open a UDP socket at a random port.
+   *
+   * @return the socket
+   * @throws IOException
+   *           if the method fails after a reasonable amount of attempts
+   */
+  public static final DatagramSocket openUDPServerSocketAtRandomPort()
+      throws IOException {
+    final Random random;
+    int maxTrials;
+
+    random = new Random();
+    maxTrials = 256;
+    for (;;) {
+      try {
+        return new DatagramSocket(1024 + random.nextInt(64512));
+      } catch (final IOException ioe) {
+        if ((--maxTrials) < 0) {
+          throw ioe;
+        }
+      }
     }
   }
 
@@ -267,16 +324,15 @@ public final class NetworkUtils {
      */
     @SuppressWarnings({ "unchecked", "rawtypes" })
     private static final ArrayListView<InetAddress> __createGlobalAddresses() {
-      final HashSet<InetAddress> addresses;
-      final ExecutorService serv;
       final String[] hosts;
-      final int size;
-      final InetAddress[] addrs;
+      final __GlobalNameCollector collector;
+      final __GlobalNameDetector[] threads;
+      final int port;
+      InetAddress[] addrs;
+      int index;
 
       // http://superuser.com/questions/420969
       // http://stackoverflow.com/questions/2939218/
-
-      addresses = new HashSet<>();
       hosts = new String[] { //
           "http://checkip.amazonaws.com/", //$NON-NLS-1$
           "http://icanhazip.com/", //$NON-NLS-1$
@@ -285,50 +341,333 @@ public final class NetworkUtils {
           "http://ipecho.net/plain", //$NON-NLS-1$
           "http://wtfismyip.com/text", //$NON-NLS-1$
       };
-      serv = Executors.newFixedThreadPool(hosts.length);
+
+      addrs = null;
       try {
-        for (final String urlStr : hosts) {
-          serv.submit(new __AddressGetterJob(addresses, urlStr));
+        collector = new __GlobalNameCollector();
+        port = collector._getPort();
+
+        threads = new __GlobalNameDetector[hosts.length];
+        for (index = hosts.length; (--index) >= 0;) {
+          threads[index] = new __GlobalNameDetector(hosts[index], port);
         }
-        serv.shutdown();
-        serv.awaitTermination(5, TimeUnit.MINUTES);
+        for (index = hosts.length; (--index) >= 0;) {
+          threads[index].join();
+          threads[index] = null;
+        }
+
+        collector._stop();
+        collector.join();
+        addrs = collector._getNames();
       } catch (final Throwable error) {
         ErrorUtils.logError(Configuration.getGlobalLogger(), Level.WARNING, //
             "Recoverable/ignoreable error while trying to obtain the internet addresses under which we are visible to the outside world.", //$NON-NLS-1$
             error, false, RethrowMode.DONT_RETHROW);
       }
-
-      synchronized (addresses) {
-        size = addresses.size();
-        if (size <= 0) {
-          return ((ArrayListView) (ArraySetView.EMPTY_SET_VIEW));
+      if (addrs != null) {
+        if (addrs.length > 0) {
+          return new ArrayListView<>(addrs);
         }
-        addrs = addresses.toArray(new InetAddress[size]);
       }
-      Arrays.sort(addrs, new __InetCmp());
-      return new ArrayListView<>(addrs);
+      return ((ArrayListView) (ArraySetView.EMPTY_SET_VIEW));
     }
   }
 
-  /** an address getter job */
-  private static final class __AddressGetterJob implements Runnable {
-    /** the set */
-    private final HashSet<InetAddress> m_set;
-    /** the host */
-    private final String m_host;
+  /** The holder class for the public address */
+  private static final class __PublicAddress {
+
+    /** the public address */
+    static final InetAddress PUBLIC_ADDRESS = __PublicAddress
+        .__getPublicAddress();
 
     /**
-     * create the address getter job
+     * Compute the public address
      *
-     * @param set
-     *          the target set
-     * @param host
-     *          the host to query
+     * @return the public address
      */
-    __AddressGetterJob(final HashSet<InetAddress> set, final String host) {
+    private static final InetAddress __getPublicAddress() {
+      ArrayListView<InetAddress> local, global;
+
+      local = NetworkUtils.getLocalAddresses();
+      global = NetworkUtils.getGlobalAddresses();
+
+      for (final InetAddress addr : global) {
+        if (local.contains(global)) {
+          return addr;
+        }
+      }
+
+      if (!(global.isEmpty())) {
+        return global.get(0);
+      }
+
+      if (local.isEmpty()) {
+        try {
+          return InetAddress.getLocalHost();
+        } catch (final Throwable lerror) {
+          ErrorUtils.logError(Configuration.getGlobalLogger(),
+              Level.WARNING, //
+              "Recoverable/ignoreable error while obtaining the local host.", //$NON-NLS-1$
+              lerror, false, RethrowMode.DONT_RETHROW);
+        }
+        return InetAddress.getLoopbackAddress();
+      }
+
+      return local.get(0);
+    }
+  }
+
+  /** The holder class for the public name */
+  private static final class __PublicName {
+    /** the public name */
+    static final String PUBLIC_NAME = NetworkUtils
+        .getInetAddressName(__PublicAddress.PUBLIC_ADDRESS);
+  }
+
+  /** A comparator for internet addressess */
+  private static final class __InetCmp implements Comparator<InetAddress> {
+
+    /** create */
+    __InetCmp() {
       super();
-      this.m_set = set;
-      this.m_host = host;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final int compare(final InetAddress o1, final InetAddress o2) {
+      return Integer.compare(__InetCmp._getVal(o1), __InetCmp._getVal(o2));
+    }
+
+    /**
+     * get the value of an internet address for comparison
+     *
+     * @param a
+     *          the address
+     * @return the value
+     */
+    @SuppressWarnings("unused")
+    static final int _getVal(final InetAddress a) {
+      int val;
+      val = 0;
+      try {
+        if (a == null) {
+          return Integer.MAX_VALUE;
+        }
+        if (a.isLoopbackAddress()) {
+          val |= 128;
+        }
+        if (a.isMulticastAddress()) {
+          val |= 64;
+        }
+        if (a.isMCNodeLocal()) {
+          val |= 32;
+        }
+        if (a.isMCLinkLocal()) {
+          val |= 16;
+        }
+        if (a.isMCGlobal()) {
+          val |= 8;
+        }
+        if (a.isSiteLocalAddress()) {
+          val |= 4;
+        }
+        if (a.isLinkLocalAddress()) {
+          val |= 2;
+        }
+        if (a.isAnyLocalAddress()) {
+          val |= 1;
+        }
+        return val;
+      } catch (final Throwable error) {
+        return (Integer.MAX_VALUE - 1);
+      }
+    }
+  }
+
+  /** the local server thread. */
+  private static final class __GlobalNameCollector extends Thread {
+    /** the internal socket */
+    private final DatagramSocket m_socket;
+    /** the hosts */
+    private HashMap<InetAddress, ___Holder> m_hosts;
+    /** are we not done yet? */
+    private volatile boolean m_alive;
+
+    /**
+     * create
+     *
+     * @throws IOException
+     *           if the socket creation fails
+     */
+    __GlobalNameCollector() throws IOException {
+      super();
+      this.m_socket = NetworkUtils.openUDPServerSocketAtRandomPort();
+      this.m_socket.setSoTimeout(2000);
+      this.m_alive = true;
+      this.start();
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void run() {
+      DatagramPacket message;
+      final byte[] buffer;
+      byte[] temp;
+      InetAddress addr;
+      int length;
+      ___Holder holder;
+
+      try {
+        buffer = new byte[16];
+        message = new DatagramPacket(buffer, 0, buffer.length);
+
+        looper: while (this.m_alive) {
+          try {
+            this.m_socket.receive(message);
+            length = message.getLength();
+            if (length > 0) {
+              if (length != buffer.length) {
+                temp = new byte[length];
+                System.arraycopy(buffer, 0, temp, 0, length);
+              } else {
+                temp = buffer;
+              }
+              addr = InetAddress.getByAddress(temp);
+              temp = null;
+
+              if (this.m_hosts == null) {
+                this.m_hosts = new HashMap<>();
+              }
+              holder = this.m_hosts.get(addr);
+              if (holder == null) {
+                holder = new ___Holder(addr);
+                this.m_hosts.put(addr, holder);
+              } else {
+                holder.m_count++;
+              }
+            }
+          } catch (@SuppressWarnings("unused") final SocketTimeoutException ste) {
+            continue looper;
+          } catch (final Throwable error) {
+            ErrorUtils.logError(Configuration.getGlobalLogger(),
+                Level.WARNING,
+                "Ignorable error while trying to receive message from helper socket for network address determination.", //$NON-NLS-1$
+                error, false, RethrowMode.DONT_RETHROW);
+            break looper;
+          }
+        }
+      } finally {
+        try {
+          this.m_socket.close();
+        } catch (final Throwable error) {
+          ErrorUtils.logError(Configuration.getGlobalLogger(),
+              Level.WARNING,
+              "Ignorable error while trying to close helper socket for network address determination.", //$NON-NLS-1$
+              error, false, RethrowMode.DONT_RETHROW);
+        }
+      }
+    }
+
+    /** stop this server */
+    final void _stop() {
+      this.m_alive = false;
+    }
+
+    /**
+     * get the port of this collector's server socket
+     *
+     * @return the port
+     */
+    final int _getPort() {
+      return this.m_socket.getLocalPort();
+    }
+
+    /**
+     * get this server's public names
+     *
+     * @return the collected names
+     */
+    final InetAddress[] _getNames() {
+      final HashMap<InetAddress, ___Holder> holders;
+      final ___Holder[] list;
+      final InetAddress[] names;
+      final int size;
+      int index;
+
+      this.m_alive = false;
+      holders = this.m_hosts;
+      this.m_hosts = null;
+
+      if (holders != null) {
+        size = holders.size();
+        if (size > 0) {
+          list = holders.values().toArray(new ___Holder[size]);
+          Arrays.sort(list);
+          names = new InetAddress[list.length];
+          for (index = 0; index < names.length; index++) {
+            names[index] = list[index].m_address;
+          }
+          return names;
+        }
+      }
+      return null;
+    }
+
+    /** the holder */
+    private static final class ___Holder implements Comparable<___Holder> {
+      /** the name */
+      final InetAddress m_address;
+      /** the number of times it was returned */
+      int m_count;
+
+      /**
+       * create the holder
+       *
+       * @param address
+       *          the address
+       */
+      ___Holder(final InetAddress address) {
+        super();
+        this.m_address = address;
+      }
+
+      /** {@inheritDoc} */
+      @Override
+      public final int compareTo(final ___Holder o) {
+        int comp;
+        if (o == this) {
+          return 0;
+        }
+        comp = Integer.compare(this.m_count, o.m_count);
+        if (comp != 0) {
+          return (-comp);
+        }
+        return Integer.compare(__InetCmp._getVal(this.m_address),
+            __InetCmp._getVal(o.m_address));
+      }
+    }
+  }
+
+  /** the global name detector thread */
+  private static final class __GlobalNameDetector extends Thread {
+    /** the url to contact */
+    private final String m_host;
+    /** the destination port to send an udp package to */
+    private final int m_destPort;
+
+    /**
+     * create the name detector thread
+     *
+     * @param url
+     *          the url to contact
+     * @param destPort
+     *          the destination port to send the answer to
+     */
+    __GlobalNameDetector(final String url, final int destPort) {
+      super();
+      this.m_host = url;
+      this.m_destPort = destPort;
+      this.start();
     }
 
     /** {@inheritDoc} */
@@ -339,6 +678,7 @@ public final class NetworkUtils {
       final InetAddress addr;
       String ip, z;
       int index;
+      byte[] address;
 
       try {
         url = new URL(this.m_host);
@@ -391,8 +731,12 @@ public final class NetworkUtils {
           }
 
           if (addr != null) {
-            synchronized (this.m_set) {
-              this.m_set.add(addr);
+            address = addr.getAddress();
+            if (address != null) {
+              try (DatagramSocket socket = new DatagramSocket()) {
+                socket.send(new DatagramPacket(address, 0, address.length,
+                    addr, this.m_destPort));
+              }
             }
           }
         }
@@ -401,114 +745,6 @@ public final class NetworkUtils {
             ((("Recoverable/ignoreable error while trying to obtain the internet address by using the website '" //$NON-NLS-1$
                 + this.m_host) + '\'') + '.'),
             inner, false, RethrowMode.DONT_RETHROW);
-      }
-    }
-  }
-
-  /** The holder class for the public address */
-  private static final class __PublicAddress {
-
-    /** the public address */
-    static final InetAddress PUBLIC_ADDRESS = __PublicAddress
-        .__getPublicAddress();
-
-    /**
-     * Compute the public address
-     *
-     * @return the public address
-     */
-    private static final InetAddress __getPublicAddress() {
-      ArrayListView<InetAddress> local, global;
-
-      local = NetworkUtils.getLocalAddresses();
-      global = NetworkUtils.getGlobalAddresses();
-
-      for (final InetAddress addr : global) {
-        if (local.contains(global)) {
-          return addr;
-        }
-      }
-
-      if (local.isEmpty()) {
-        try {
-          return InetAddress.getLocalHost();
-        } catch (final Throwable lerror) {
-          ErrorUtils.logError(Configuration.getGlobalLogger(),
-              Level.WARNING, //
-              "Recoverable/ignoreable error while obtaining the local host.", //$NON-NLS-1$
-              lerror, false, RethrowMode.DONT_RETHROW);
-        }
-        return InetAddress.getLoopbackAddress();
-      }
-
-      return local.get(0);
-    }
-  }
-
-  /** The holder class for the public name */
-  private static final class __PublicName {
-    /** the public name */
-    static final String PUBLIC_NAME = NetworkUtils
-        .getInetAddressName(__PublicAddress.PUBLIC_ADDRESS);
-  }
-
-  /** A comparator for internet addressess */
-  private static final class __InetCmp implements Comparator<InetAddress> {
-
-    /** create */
-    __InetCmp() {
-      super();
-    }
-
-    /** {@inheritDoc} */
-    @Override
-    public final int compare(final InetAddress o1, final InetAddress o2) {
-      return Integer.compare(__InetCmp.__getVal(o1),
-          __InetCmp.__getVal(o2));
-    }
-
-    /**
-     * get the value of an internet address for comparison
-     *
-     * @param a
-     *          the address
-     * @return the value
-     */
-    @SuppressWarnings("unused")
-    private static final int __getVal(final InetAddress a) {
-      int val;
-      val = 0;
-      try {
-        if (a == null) {
-          return Integer.MAX_VALUE;
-        }
-        if (a.isLoopbackAddress()) {
-          val |= 128;
-        }
-        if (a.isMulticastAddress()) {
-          val |= 64;
-        }
-        if (a.isMCNodeLocal()) {
-          val |= 32;
-        }
-        if (a.isMCLinkLocal()) {
-          val |= 16;
-        }
-        if (a.isMCGlobal()) {
-          val |= 8;
-        }
-        if (a.isSiteLocalAddress()) {
-          val |= 4;
-        }
-        if (a.isLinkLocalAddress()) {
-          val |= 2;
-        }
-        if (a.isAnyLocalAddress()) {
-          val |= 1;
-        }
-        return val;
-      } catch (final Throwable error) {
-        return (Integer.MAX_VALUE - 1);
       }
     }
   }
--- src/test/java/examples/snippets/NetworkUtilsTest.java
+++ src/test/java/examples/snippets/NetworkUtilsTest.java
@@ -20,17 +20,31 @@ public class NetworkUtilsTest {
    *          the command line arguments
    */
   public static final void main(final String[] args) {
+    System.out.println("Local Addresses: "); //$NON-NLS-1$
     for (final InetAddress addr : NetworkUtils.getLocalAddresses()) {
-      System.out.println(
-          addr.toString() + '\t' + NetworkUtils.getInetAddressName(addr));
+      System.out.print(' ');
+      System.out.print('(');
+      System.out.print(addr);
+      System.out.print(',');
+      System.out.print(' ');
+      System.out.print(NetworkUtils.getInetAddressName(addr));
+      System.out.print(')');
     }
-    System.out.println(" xxxxxxxxxxxxxxxxxxxxxx "); //$NON-NLS-1$
+    System.out.println();
+    System.out.println("Global addresses: "); //$NON-NLS-1$
     for (final InetAddress addr : NetworkUtils.getGlobalAddresses()) {
-      System.out.println(
-          addr.toString() + '\t' + NetworkUtils.getInetAddressName(addr));
+      System.out.print(' ');
+      System.out.print('(');
+      System.out.print(addr);
+      System.out.print(',');
+      System.out.print(' ');
+      System.out.print(NetworkUtils.getInetAddressName(addr));
+      System.out.print(')');
     }
-    System.out.println(" xxxxxxxxxxxxxxxxxxxxxx "); //$NON-NLS-1$
+    System.out.println();
+    System.out.print("Most likely public address: "); //$NON-NLS-1$
     System.out.println(NetworkUtils.getMostLikelyPublicAddress());
+    System.out.print("Most likely public name: "); //$NON-NLS-1$
     System.out.println(NetworkUtils.getMostLikelyPublicName());
   }
 }

5edcd0d6c144fba8df1afefbdcbb86d568767526
Thomas Weise <tweise@home>
2016-06-02 17:38:18 +0800

    Added Location-Specific Evaluate Methods to IFittingQualityMeasure
    This should help us to first fit functions for a few selected points and
    then fit for the whole point set. This is intended to help us to improve
    the overall fitting quality.

--- src/main/java/org/optimizationBenchmarking/utils/ml/fitting/spec/IFittingQualityMeasure.java
+++ src/main/java/org/optimizationBenchmarking/utils/ml/fitting/spec/IFittingQualityMeasure.java
@@ -33,4 +33,46 @@ public interface IFittingQualityMeasure {
    */
   public abstract void evaluate(final ParametricUnaryFunction model,
       final double[] parameters, final FittingEvaluation dest);
+
+  /**
+   * Obtain the number of points underlying the fitting quality measure.
+   *
+   * @return the number of points underlying the fitting quality measure
+   */
+  public abstract int getPointCount();
+
+  /**
+   * Compute the quality of the given model parameters by only considering
+   * the points at the specified indices.
+   *
+   * @param model
+   *          the model to be assessed
+   * @param parameters
+   *          the model parameters
+   * @param points
+   *          the points at which to evaluate the parameters
+   * @return the quality
+   */
+  public abstract double evaluateAt(final ParametricUnaryFunction model,
+      final double[] parameters, final int... points);
+
+  /**
+   * Compute the quality, Jacobian, residuals, root-mean-square error and
+   * root-square error of a given set of model parameters by only
+   * considering the points at the specified indices. The member variables
+   * of {@code dest} are re-used if they are not {@code null} and of the
+   * right dimension. Otherwise they may be overwritten.
+   *
+   * @param model
+   *          the model
+   * @param parameters
+   *          the model parameters
+   * @param dest
+   *          the destination record.
+   * @param points
+   *          the points at which to evaluate the parameters
+   */
+  public abstract void evaluateAt(final ParametricUnaryFunction model,
+      final double[] parameters, final FittingEvaluation dest,
+      final int... points);
 }

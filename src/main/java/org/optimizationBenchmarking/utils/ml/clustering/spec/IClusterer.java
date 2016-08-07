1387841723764380ae392ad42ebdd4087d140571
Thomas Weise <tweise@home>
2016-08-07 16:48:28 +0800

    IClusterer and IFunctionFitter now extend ISemanticComponent

--- src/main/java/org/optimizationBenchmarking/utils/ml/clustering/spec/IClusterer.java
+++ src/main/java/org/optimizationBenchmarking/utils/ml/clustering/spec/IClusterer.java
@@ -1,9 +1,10 @@
 package org.optimizationBenchmarking.utils.ml.clustering.spec;
 
+import org.optimizationBenchmarking.utils.document.spec.ISemanticComponent;
 import org.optimizationBenchmarking.utils.tools.spec.ITool;
 
 /** The clustering tool */
-public interface IClusterer extends ITool {
+public interface IClusterer extends ITool, ISemanticComponent {
 
   /** {@inheritDoc} */
   @Override
--- src/main/java/org/optimizationBenchmarking/utils/ml/fitting/spec/IFunctionFitter.java
+++ src/main/java/org/optimizationBenchmarking/utils/ml/fitting/spec/IFunctionFitter.java
@@ -1,12 +1,13 @@
 package org.optimizationBenchmarking.utils.ml.fitting.spec;
 
+import org.optimizationBenchmarking.utils.document.spec.ISemanticComponent;
 import org.optimizationBenchmarking.utils.tools.spec.ITool;
 
 /**
  * The basic interface for function fitters, i.e., software components
  * which can fit a model to a given data set.
  */
-public interface IFunctionFitter extends ITool {
+public interface IFunctionFitter extends ITool, ISemanticComponent {
 
   /** {@inheritDoc} */
   @Override
